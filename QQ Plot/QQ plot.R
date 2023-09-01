6241ef5c903a8fe02c28473bf138569e0015c2d8
Vladislav Pyatnitskiy <111236801+vladislavpyatnitskiy@users.noreply.github.com>
2023-09-01 23:56:26 +0500

    Update QQ plot.R
    Updated qq plot generation
    
    * modified script so if it can generate for more than one qq plot from dataset
    * Plots are generated with the ticker of data
    * line is wider and can be changed if needed

--- QQ Plot/QQ plot.R
+++ QQ Plot/QQ plot.R
@@ -27,8 +27,32 @@ portfolioReturns <-as.timeSeries(portfolioPrices)
 
 portfolioReturns
 
-lrtn=diff(log(portfolioReturns))
-lrtn <- lrtn[-1,]
+qqgraph <- function(x){
+  # Calculate Returns
+  x=diff(log(x))
+  
+  # Get rid of NA
+  x <- x[-1,]
+  
+  # Loop enables to generate multiple plots if there are more than 1   
+  for (n in 1:ncol(x)){
+    # Take ticker from Time Series to reflect on plot
+    qqstocknames <- colnames(x[,n])
+    
+    # Create variable to reflect in main section of plot settings
+    qqmain <- sprintf("%s Q-Q Plot", qqstocknames)
+    
+    # Plot QQ plot
+    qqnorm(x[,n],
+           main = qqmain,
+           sub = "Source: Yahoo Finance"
+    )
+    
+    # Add line
+    qqline(x[,n],
+           col = "red",
+           lwd = 3)
+  }
+}
 
-qqnorm(lrtn)
-qqline(lrtn, col = "red")
+qqgraph(portfolioReturns)
