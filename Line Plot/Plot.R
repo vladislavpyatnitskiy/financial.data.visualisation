e2c8b56a4b7e8f790ddcf89230baa69f86b5dda7
Vladislav Pyatnitskiy <111236801+vladislavpyatnitskiy@users.noreply.github.com>
2023-09-02 23:56:32 +0500

    Update Plot.R
    Added comments

--- Line Plot/Plot.R
+++ Line Plot/Plot.R
@@ -1,8 +1,16 @@
 plot_data <- function(x){
+  # For each column in data set
   for (n in 1:ncol(x)){
+    # Take names from columns
     plot_data_names <- colnames(x[,n])
+    
+    # Put them in string to be reflected in the main of the plot
     main_plot_data <- sprintf("%s Stock Performance", plot_data_names)
+    
+    # And on the ylab as well
     ylab_plot_data <- sprintf("%s Prices", plot_data_names)
+    
+    # Plot finally
     plot(x[,n],
          main = main_plot_data,
          sub = "Source: Yahoo! Finance",
