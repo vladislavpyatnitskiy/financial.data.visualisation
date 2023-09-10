8727796e106da9c722f8444f3eab386ae5651175
Vladislav Pyatnitskiy <111236801+vladislavpyatnitskiy@users.noreply.github.com>
2023-09-10 21:01:12 +0500

    Create Alternative script for Dual Plot.R
    Added optional script for generating Dual Axis Plot

---
+++ Dual Axis Chart/Alternative script for Dual Plot.R
@@ -0,0 +1,40 @@
+# library to use
+library(latticeExtra)
+
+# Function 
+dual_axis_plot <- function(x){
+  
+  # Set up array for x-axis
+  dates_for_dual_plot <- 1:nrow(x) 
+  
+  # Set up array for first y-axis
+  first_dual_part <- as.matrix(x[,1])
+  
+  # Set up array for second y-axis
+  second_dual_part <- as.matrix(x[,2])
+  
+  # Add all of them into one df
+  data_for_dual_plot <- data.frame(dates_for_dual_plot,
+                                   first_dual_part,
+                                   second_dual_part)
+  
+  # First data to plot
+  obj1 <- xyplot(first_dual_part ~ dates_for_dual_plot,
+                 data_for_dual_plot,
+                 ylab = "Stock A",
+                 xlab = "Trading Days",
+                 main = "Stock Performance Comparison",
+                 type = "l" , lwd=2, col="steelblue",
+                 sub = "Source: Yahoo! Finance")
+  
+  # Second data to plot
+  obj2 <- xyplot(second_dual_part ~ dates_for_dual_plot,
+                 data_for_dual_plot,
+                 ylab = "Stock B",
+                 type = "l", lwd=2, col="#69b3a2")
+  
+  # Make the plot with second y axis:
+  doubleYScale(obj1, obj2, add.ylab2 = TRUE)
+}
+# Test 
+dual_axis_plot(portfolioReturns)
