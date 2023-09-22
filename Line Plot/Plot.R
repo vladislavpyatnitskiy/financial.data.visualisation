e4f5a08f075dd54b508d324b2a792eff3ad3b981
Vladislav Pyatnitskiy <111236801+vladislavpyatnitskiy@users.noreply.github.com>
2023-09-22 19:07:32 +0500

    Update Plot.R
    Optimised script so it produces the same result using less characters and rows:
    
    • Put sequence in for loop
    • Reduced "Stock" word so it can be applied to other securities
    • Put "sprintf" into plot function

--- Line Plot/Plot.R
+++ Line Plot/Plot.R
@@ -1,22 +1,17 @@
+# Function to create plots from each data set column 
 plot_data <- function(x){
+  
   # For each column in data set
-  for (n in 1:ncol(x)){
-    # Take names from columns
-    plot_data_names <- colnames(x[,n])
-    
-    # Put them in string to be reflected in the main of the plot
-    main_plot_data <- sprintf("%s Stock Performance", plot_data_names)
-    
-    # And on the ylab as well
-    ylab_plot_data <- sprintf("%s Prices", plot_data_names)
-    
-    # Plot finally
+  for (n in seq(colnames(x))){
+
+    # Plot
     plot(x[,n],
-         main = main_plot_data,
-         sub = "Source: Yahoo! Finance",
-         xlab = "Trading Days",
-         ylab = ylab_plot_data
-         )
+         main=sprintf("%s Performance", colnames(x[,n])), # Title of Security
+         sub="Source: Yahoo! Finance",
+         xlab="Trading Days",
+         ylab=sprintf("%s Prices", colnames(x[,n])) # Prices of Security
+    )
   }
 }
+# Test
 plot_data(stock_data)
