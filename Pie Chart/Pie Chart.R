ea02bb0dfadc1a12e80aafe905a3b96c90ec692c
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-09-25 23:58:01 +0500

    Update Pie Chart.R
    Optimised script to generate Pie Plot:
    
    • Transformed to function
    • Percentages, labels/tickers, main and subs are typed in function round brackets

--- Pie Chart/Pie Chart.R
+++ Pie Chart/Pie Chart.R
@@ -1,24 +1,29 @@
-# Stock Portion in Pie Chart
-slices <- c(0.278716291651169, 0.411679570881108, 0.0727978571595576, 0.176817615423501, 0.0599886648846643)
+# Function to facilitate Pie Plot generation
+piePlot <- function(slices, tickers, main = NULL, sub = NULL){
+  
+  # Percentage calculation
+  pct <- round(slices / sum(slices)*100)
+  
+  # Add percents to labels 
+  lbls <- paste(tickers, pct) 
+  
+  # Add % to labels 
+  lbls <- paste(lbls,"%",sep="") 
+  
+  # Pie Chart configuration
+  pie(slices,labels = lbls,
+      col = rainbow(length(lbls)),
+      main = main, 
+      mtext = "hdds")
+  pie(slices,
+      labels = lbls,
+      main = main,
+      sub = sub) 
+}
 
-# Stock Tickers
-lbls <- c("BELU", "BSPB", "IRAO", "LKOH", "RTKM")
-
-# Percentage calculation
-pct <- round(slices/sum(slices)*100)
-
-# Add percents to labels 
-lbls <- paste(lbls, pct) 
-
-# Add % to labels 
-lbls <- paste(lbls,"%",sep="") 
-
-# Pie Chart configuration
-pie(slices,labels = lbls,
-    col=rainbow(length(lbls)),
-    main="Dividend Yield Distribution",
-    mtext = "hdds")
-pie(slices,
-    labels = lbls,
-    main="Dividend Yield Distribution",
-    sub = "Source: Investing.com")
+# Test
+piePlot(slices = c(0.278716291651169, 0.411679570881108, 0.0727978571595576,
+                   0.176817615423501, 0.0599886648846643),
+        tickers = c("BELU", "BSPB", "IRAO", "LKOH", "RTKM"),
+        main = "Dividend Yield Distribution",
+        sub = "Source: Investing.com")
