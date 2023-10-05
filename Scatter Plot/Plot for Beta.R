b585fbb6eb70e51acae6843cdff7e333d42ade83
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-10-05 23:01:01 +0500

    Update Plot for Beta.R
    Optimised script
    
    • Reduced excessive script, such as:
    
    1. Separate Benchmark
    2. Yahoo download
    
    • Integrated sprintf and colnames into plot script
    • Added option to change benchmark so it is possible to create scatter plot with other indices as well

--- Scatter Plot/Plot for Beta.R
+++ Scatter Plot/Plot for Beta.R
@@ -13,22 +13,18 @@ character.only = TRUE
 tickers_for_test <- c("AIG", "MET", "UNM", "HIG", "OMF")
 
 # Function to visualise Beta Scatter Plot
-betaplot <- function(y, z = NULL, i = NULL){
-  
-  # Define Yahoo ticker for S&P 500
-  spx <- "^GSPC"
-  
-  # Add 10 year Treasuries to list
-  y <- c(y, spx)
+betaplot <- function(y, bnchm = "^GSPC", start_date = NULL, end_date = NULL){
+
+  # Add benchmark to list
+  y <- c(y, bnchm)
   
   # Create an empty variable
   portfolioPrices <- NULL
   
   # Loop for data extraction
   for (Ticker in y){
-           
     # Set up statements for start and end dates
-    if (is.null(z) && is.null(i)) {
+    if (is.null(start_date) && is.null(end_date)) {
       # When neither start date nor end date are defined
       portfolioPrices <- cbind(portfolioPrices,
                                getSymbols(Ticker,
@@ -36,25 +32,15 @@ betaplot <- function(y, z = NULL, i = NULL){
                                           to = Sys.Date(),
                                           src = "yahoo",
                                           auto.assign=FALSE)[,4])
-    } else if (is.null(i)) {
-      # When only start date is defined
-      portfolioPrices <- cbind(portfolioPrices,
-                               getSymbols(Ticker, from = z, src = "yahoo",
-                                          auto.assign=FALSE)[,4])
-    } else if (is.null(z)) {
-      # When only end date is defined
-      portfolioPrices <- cbind(portfolioPrices,
-                               getSymbols(Ticker, to = i, src = "yahoo",
-                                          auto.assign=FALSE)[,4])
     } else { 
       # When both start date and end date are defined
       portfolioPrices <- cbind(portfolioPrices,
-                               getSymbols(Ticker, from = z, to = i,
+                               getSymbols(Ticker,
+                                          from = start_date,
+                                          to = end_date,
                                           src = "yahoo", 
-                                          auto.assign=FALSE)[,4])
-    }
+                                          auto.assign=FALSE)[,4]) }
   }
-         
   # Get rid of NAs
   portfolioPrices <- portfolioPrices[apply(portfolioPrices,1,
                                            function(x) all(!is.na(x))),]
@@ -70,40 +56,25 @@ betaplot <- function(y, z = NULL, i = NULL){
   # Calculate Returns and get rid of NA
   x=diff(log(portfolioReturns))[-1,]
   
-  # Copy column S&P 500 column and make separate column
-  spx <- x[,"^GSPC"]
+  # Copy column index column and make separate column
+  spx <- x[,bnchm]
   
-  # Subset S&P 500 from data set
-  stock_returns <- x[, -which(names(x) == "^GSPC")]
+  # Subset index from data set
+  stock_returns <- x[, -which(names(x) == bnchm)]
   
-  # Loop enables to generate multiple plots if there are more than 1   
-  for (n in 1:ncol(stock_returns)) {
-    
-    # Run a regression (CAPM) 
-    fit <- lm(stock_returns[,n] ~ spx)
-    
-    # Take ticker from Time Series to reflect on plot
-    name_beta_stock <- colnames(stock_returns[,n])
-    
-    # Create variable to reflect in ylab section of plot settings
-    beta_y_lab <- sprintf("%s Return (%%)", name_beta_stock)
-    
-    # Create variable to reflect in main section of plot settings
-    beta_main <- sprintf("%s Beta", name_beta_stock)
+  # Loop generates multiple plots if there are more than 1 for each column   
+  for (n in 1:ncol(stock_returns)) { security <- stock_returns[,n]
     
     # Create Scatter Plot
     plot_for_beta <- plot(spx,
-                          stock_returns[,n],
-                          ylab=beta_y_lab,
+                          security,
+                          ylab=sprintf("%s Return (%%)", colnames(security)),
                           xlab="Market Return (%)",
-                          main=beta_main,
+                          main=sprintf("%s Beta", colnames(security)),
                           sub = "Source: Yahoo Finance",
-                          las = 1
-    )
-    
+                          las = 1)
     # Draw a trend line
-    abline_for_beta <- abline(fit, col = "red", lwd = 3)
-  }
+    abline_for_beta <- abline(lm(security ~ spx), col = "red", lwd = 3) }
 }
 # Test
-betaplot(tickers_for_test)
+betaplot(tickers_for_test, bnchm = "^GSPC")
