b2ba4a3e5f94cefbdf837f1d2a93440603e700a8
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-10-06 18:08:23 +0500

    Create 3D Scatter Plot.R
    Added script for 3D Scatter Plot

---
+++ Scatter Plot/3D Scatter Plot.R
@@ -0,0 +1,15 @@
+# Main script
+fig <- plot_ly(Basic_Materials, x = ~Basic_Materials$`Standard Deviation`,
+        y = ~Basic_Materials$Beta, z = ~Basic_Materials$Return,
+        size = ~Basic_Materials$MarketCap, color=~Basic_Materials$Industry,
+        marker = list(symbol = 'circle', sizemode = 'diameter'),
+        sizes = c(5, 150),
+        text = ~paste('Ticker:', Basic_Materials$Ticker))
+
+# Rename axes
+fig <- fig %>% layout(title = 'Basic Materials',
+                      scene = list(xaxis = list(title = 'Standard Deviation'),
+                                   yaxis = list(title = 'Beta'),
+                                   zaxis = list(title = 'Return')))
+# Display
+fig
