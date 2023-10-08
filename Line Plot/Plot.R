5b94f9903d6f1e05e14c69643614ebb5203c9415
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-10-08 14:11:18 +0500

    Update Plot.R
    Reduced excessive script and decreased space

--- Line Plot/Plot.R
+++ Line Plot/Plot.R
@@ -4,23 +4,18 @@ plot_data <- function(x, lg = F){
   # Make logs if needed
   if (isTRUE(lg)) { x = diff(log(x))[-1,] }
   
-  # For each column in data set
-  for (n in seq(colnames(x))){
-    
-    # Define column
-    security <- x[,n]
+  # For each column in data set define column
+  for (n in seq(colnames(x))){ security <- x[,n]
     
     # Plot
     plot(security,
          main=sprintf("%s Performance", colnames(security)), # Security Title
          sub="Source: Yahoo! Finance",
          xlab="Trading Days",
-         ylab=sprintf("%s Prices", colnames(security)) # Security Prices
-    )
+         ylab=sprintf("%s Prices", colnames(security)))
     
     # Add horizontal line
-    abline(h = 0)
-  }
+    abline(h = 0) }
 }
 # Test
 plot_data(returns_df)
