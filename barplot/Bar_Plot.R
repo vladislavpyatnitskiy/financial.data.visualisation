77806eb1c1995aec452245dba8eadd8ca3aeb7e2
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-10-28 22:23:19 +0500

    Update Bar_Plot.R
    * Optimised and Upgraded script:
    
    • Improved return calculation using and combining apply, cumulative sum, log and sort in one operation
    • Changed function to show names on barplot
    • Added grey dotted lines for better plot perception
    • Configured x axis for better perception

--- barplot/Bar_Plot.R
+++ barplot/Bar_Plot.R
@@ -1,31 +1,32 @@
 # Barplot
-brplt <- function(x, y, col = "blue", main = NULL, xlim = NULL){
-    # Define the time period
-    x <- merge(x[y,],
-                  x[nrow(x),])
-    
-    # Calculate the change and remove NA
-    x=diff(log(x))[-1,]
-    
-    #
-    colnames(x)
-    x1 <- as.data.frame(x)
-    x1 <- sort(x1, decreasing = T)
-    tickers_for_barplot <- colnames(x1)
-    x <- as.numeric(x)
-    x <- sort(x, decreasing = T)
-    
-    # Create barplot
-    barplot(x,
-            names.arg = tickers_for_barplot,
-            horiz = T,
-            las=1,
-            col = col,
-            main = main,
-            sub = "Source: Yahoo! Finance",
-            xlab = "Returns",
-            ylab = "",
-            xlim = xlim
-    )
-    
-  }
+bar.plt <- function(x, col = "blue", main = NULL){
+  
+  # Data Cleaning
+  x<-sort(apply(x,2,function(col) exp(sum(diff(log(col))[-1]))-1),decreasing=T)
+  
+  # Barplot
+  bar.plt.script <- barplot(x,
+                            names.arg = names(x),
+                            horiz = T,
+                            las=1,
+                            col = col,
+                            main = main,
+                            sub = "Data Source: Yahoo! Finance",
+                            xlab = "Returns",
+                            ylab = "",
+                            xlim = c(signif(min(x), 1),
+                                     signif(max(x), 1)))
+                    
+  # Add grey dotted lines
+  abline(h = bar.plt.script, col = "grey",lty = 3) # through bars
+  
+  p.seq <- seq(signif(min(x), 1), signif(max(x), 1), by = .1)
+  
+  for (n in p.seq){ abline(v = n, col = "grey", lty = 3) } # vertical bars
+  
+  axis(side = 1, at = p.seq, las = 1) # configure x axis
+  
+  box() # Make borders for plot
+}
+# Test
+bar.plt(stock_data, main = "Asset Performance")
