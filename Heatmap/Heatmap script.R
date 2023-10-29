d3a60cf28da8f5c2c13b5a0323ac91cafff52f08
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-10-29 16:24:49 +0500

    Update Heatmap script.R
    Optimised and upgraded script:
    
    • Added option to choose size of correlation values
    • Combined operations
    • Renamed variables

--- Heatmap/Heatmap script.R
+++ Heatmap/Heatmap script.R
@@ -1,31 +1,19 @@
 # Function to create heatmap
-heatmap.plt <- function(data){
+heatmap.plt <- function(data, size = 1.5){
   
   m.correlation = as.matrix(data) # Convert data into matrix
   
   c.correlation = ncol(m.correlation) # Get number of columns
   
-  # Cut column and row names if there are longer than 4 characters
-  names_for_correlation = abbreviate(colnames(m.correlation), 4)
-  
   new_cor <- cor(m.correlation) # Calculate correlation coefficients
   
   # Create appropriate colour for each pair of correlation for heatmap
-  ncolors_for_cor <- 10 * length(unique(as.vector(new_cor)))
-  k_for_corr <- round(ncolors_for_cor/2)
-  r_for_corr <- c(rep(0, k_for_corr), seq(0, 1, length = k_for_corr))
-  g_for_corr <- c(rev(seq(0, 1, length = k_for_corr)), rep(0, k_for_corr))
-  b_for_corr <- rep(0, 2 * k_for_corr)
-  corrColorMatrix <- (rgb(r_for_corr, g_for_corr, b_for_corr))
-  
+  k.c <- round((10 * length(unique(as.vector(new_cor))))/2)
+  corrColorMatrix <- rgb(c(rep(0, k.c), seq(0, 1, length = k.c)),
+                         c(rev(seq(0,1,length=k.c)), rep(0,k.c)), rep(0,2*k.c))
   # Display heatmap
-  image(x = 1:c.correlation,
-        y = 1:c.correlation,
-        z = new_cor[, c.correlation:1],
-        col = corrColorMatrix,
-        axes = FALSE, main = "",
-        xlab = "",
-        ylab = "")
+  image(x = 1:c.correlation,y = 1:c.correlation,z = new_cor[, c.correlation:1],
+        col = corrColorMatrix, axes = FALSE, main = "", xlab = "", ylab = "")
   
   # Add labels for both axis
   axis(2, at = c.correlation:1, labels = colnames(m.correlation), las = 2)
@@ -39,14 +27,12 @@ heatmap.plt <- function(data){
   x = y = 1:c.correlation
   n_x = n_y = length(y)
   xoy = cbind(rep(x, n_y), as.vector(matrix(y, n_x, n_y, byrow = TRUE)))
-  coord_for_corr = matrix(xoy, n_x * n_y, 2, byrow = FALSE)
-  X_for_corr = t(new_cor)
-  for (i in 1:(c.correlation ^ 2)) {
-    text(coord_for_corr[i, 1],
-         coord_for_corr[c.correlation * c.correlation + 1 - i, 2],
-         round(X_for_corr[coord_for_corr[i,1], coord_for_corr[i, 2]],digits=2),
-         col = "white",
-         cex = 1.5) }
+  corr.coord = matrix(xoy, n_x * n_y, 2, byrow = FALSE)
+  X.corr = t(new_cor)
+  for (i in 1:c.correlation ^ 2) {
+    text(corr.coord[i, 1], corr.coord[c.correlation ^ 2 + 1 - i, 2],
+         round(X.corr[corr.coord[i,1],corr.coord[i,2]],digits=2),col = "white",
+         cex=size) }
 }
 # Test
-heatmap.plt(data = stock_log_rets)
+heatmap.plt(data = stock_log_rets, size = 1.5)
