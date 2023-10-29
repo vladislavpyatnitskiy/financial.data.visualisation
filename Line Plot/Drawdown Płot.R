dc8debe7d5e875daba0f05e26dc02e4bfbc7e065
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-10-29 16:44:53 +0500

    Update Drawdown Płot.R
    Optimised and upgraded script:
    
    • Combined operations
    • Upgraded y axis
    • Added grey dotted horizontal lines for better plot perception

--- Line Plot/Drawdown Płot.R
+++ Line Plot/Drawdown Płot.R
@@ -1,20 +1,11 @@
 # Function to plot asset drawdown
-drawdown.plt <- function(x){
+drawdown.plt <- function(x){ x <- diff(log(x)) # Log returns
   
-  # Log returns
-  x <- diff(log(x))
+  x[1,] <- 0 # Assign first value as 0
   
-  # Assign first value as 0
-  x[1,] <- 0
-  
-  # Calculate exponential sum
-  x <-apply(x, 2, function(col) (exp(cumsum(col))-1))
-  
-  # Transform into time series
-  x <- as.timeSeries(x)
-  
-  # Reduce postive values
-  x[x > 0] <- 0
+  # Calculate exponential sum and transform into time series
+  x <-as.timeSeries(apply(x, 2, function(col) exp(cumsum(col))-1))
+  x[x > 0] <- 0 # Reduce postive values
   
   # For each column in data frame
   for (n in 1:ncol(x)){ security <- x[,n]
@@ -25,7 +16,11 @@ drawdown.plt <- function(x){
          main = sprintf("%s Drawdown", colnames(security)),
          xlab = "Trading Days",
          ylab = "Negative Return (%)",
-         las = 2) }
+         las = 2,
+         ylim = c(signif(min(security), 1), 0)) }
+  
+  # Add horizontal lines
+  abline(h = seq(signif(min(security), 1), 0, 0.1), lty = 3, col = "grey")
 }
 # Test
 drawdown.plt(stock_data)
