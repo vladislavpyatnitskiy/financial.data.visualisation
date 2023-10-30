d5faf1f96625e1a68d9e8eb3d5f8c8f309290cbc
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-10-30 11:44:53 +0500

    Update QQ plot.R
    Optimised and upgraded script:
    
    • Renamed variables
    • Combined operations
    • Reduced excessive lines
    • Added horizontal and vertical grey dotted lines

--- QQ Plot/QQ plot.R
+++ QQ Plot/QQ plot.R
@@ -1,25 +1,19 @@
 # Function to generate Q-Q plot
-qqgraph <- function(x, lg = F){
+qq.plt <- function(x, lg = F){
   
-  # Calculate Returns and remove NA if applicable
-  if (isTRUE(lg)) { x = diff(log(x))[-1,] }
+  if (isTRUE(lg)) { x = diff(log(x))[-1,] } # Calculate Returns and remove NA
   
-  # Loop enables to generate multiple plots if there are more than 1   
-  for (n in 1:ncol(x)){
+  for (n in 1:ncol(x)){ s <- x[,n] # For each asset make qq plot
     
-    # Current Column
-    security <- x[,n]
+    qqnorm(s, main = sprintf("%s Q-Q Plot", colnames(s)), las = 1) # QQ plot
     
-    # Plot QQ plot
-    qqnorm(security,
-           main = sprintf("%s Q-Q Plot", colnames(security)),
-           sub = "Source: Yahoo Finance")
+    axis(side = 2, at=seq(-1, 1, 0.01), las = 1) # Y-axis with 0.01 intervals
     
-    # Add line
-    qqline(security,
-           col = "red",
-           lwd = 3)
-  }
+    qqline(s, col = "red", lwd = 3) } # Add line
+    
+  # Add grey dotted vertical & horizontal lines
+  for (n in seq(-3, 3, 1)){ abline(v = n, col = "grey", lty = 3) }
+  for (n in seq(-1, 1, 0.01)){ abline(h = n, col = "grey", lty = 3) }
 }
 # Test
-qqgraph(returns_df)
+qq.plt(returns_df)
