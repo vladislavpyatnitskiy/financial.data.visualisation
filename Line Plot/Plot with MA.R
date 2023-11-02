51c82d39aaf4ad9b5a4c2abe20c45e533a53a4df
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-11-02 21:58:11 +0500

    Update Plot with MA.R
    Optimised script:
    
    • Reduced excessive lines
    • Combined operations
    • Renamed variables

--- Line Plot/Plot with MA.R
+++ Line Plot/Plot with MA.R
@@ -1,47 +1,29 @@
-lapply(c("quantmod",
-         "ggplot2",
-         "tidyverse",
-         "timeSeries"),
-       require,
-       character.only = TRUE)
+lapply(c("quantmod", "ggplot2", "tidyverse", "timeSeries"),
+       require, character.only = TRUE)
 
 # Chart with Moving Averages
-chart_with_ma <- function(tickers, start_date = NULL,
-                          end_date = NULL, data = T){ 
+chart_with_ma <- function(tickers, s = NULL, e = NULL, data = T){ 
   
   # When data is needed create empty variable to contain values
-  if (isFALSE(data)){ portfolioPrices <- NULL
+  if (isFALSE(data)){ p <- NULL
   
   # Data download
-  for (Ticker in tickers) 
-    portfolioPrices <- cbind(portfolioPrices,
-                             getSymbols(Ticker,
-                                        from = start_date,
-                                        to = end_date,
-                                        src = "yahoo",
-                                        auto.assign=FALSE)[,4])
+  for (Ticker in tickers){ 
+    p <- cbind(p,getSymbols(Ticker,from=s,to=e,src="yahoo",auto.assign=F)[,4])}
   
-  # Remove NAs
-  portfolioPrices <- portfolioPrices[apply(portfolioPrices,1,
-                                           function(x) all(!is.na(x))),]
-  # Give tickers to data
-  colnames(portfolioPrices) <- tickers
+  p <- p[apply(p, 1, function(x) all(!is.na(x))),] # Remove NA
   
-  # Make data discrete
-  portfolioReturns <- ROC(portfolioPrices, type = "discrete")
+  colnames(p) <- tickers # Give tickers to data
   
-  # Make data time series
-  portfolioReturns <-as.timeSeries(portfolioPrices) }
+  r <-as.timeSeries(p) } # Make data time series
   
-  # For each column in data set
-  for (n in 1:ncol(portfolioReturns)){ security <- portfolioReturns[,n] 
-
-    # Create chart itself
-    new_chart_series_with_MA <- chartSeries(round(security, 2),
-                         name = sprintf("%s Stock Performance",
-                        colnames(security)), theme = "white", # colour of plot
-                      TA="addEMA(50, col='purple');addEMA(200, col='red')") }
+  # For each column in data set # Create chart itself
+  for (n in 1:ncol(r)){
+  
+    chartSeries(round(r[,n], 2),
+                name=sprintf("%s Stock Performance",colnames(r[,n])),
+                theme = "white",
+                TA="addEMA(50, col='purple');addEMA(200, col='red')") }
 }
 # Test
-chart_with_ma(tickers = "OMF", start_date = "2022-01-01",
-              end_date = "2023-01-01", data = F)
+chart_with_ma(tickers = "OMF", s = "2022-01-01", e = "2023-01-01", data = F)
