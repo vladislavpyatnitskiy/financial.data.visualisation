48b8f294035471747ec8079b1e462c93ef560f87
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-11-04 16:24:17 +0500

    Update QQ plot.R
    Reduced excessive lines

--- QQ Plot/QQ plot.R
+++ QQ Plot/QQ plot.R
@@ -1,19 +1,16 @@
 # Function to generate Q-Q plot
-qq.plt <- function(x, lg = F){
-  
-  if (isTRUE(lg)) { x = diff(log(x))[-1,] } # Calculate Returns and remove NA
+qq.plt <- function(x, lg = F){ if (isTRUE(lg)) { x = diff(log(x))[-1,] }
   
   for (n in 1:ncol(x)){ s <- x[,n] # For each asset make qq plot
-    
-    qqnorm(s, main = sprintf("%s Q-Q Plot", colnames(s)), las = 1) # QQ plot
-    
-    axis(side = 2, at=seq(-1, 1, 0.01), las = 1) # Y-axis with 0.01 intervals
-    
-    qqline(s, col = "red", lwd = 3) } # Add line
-    
+  
+  qqnorm(s, main = sprintf("%s Q-Q Plot", colnames(s)), las = 1) # QQ plot
+  
+  axis(side = 2, at=seq(-1, 1, .01), las = 1) # Y-axis with 0.01 intervals
+  
+  qqline(s, col = "red", lwd = 3) } # Add line
+  
   # Add grey dotted vertical & horizontal lines
   for (n in seq(-3, 3, 1)){ abline(v = n, col = "grey", lty = 3) }
-  for (n in seq(-1, 1, 0.01)){ abline(h = n, col = "grey", lty = 3) }
+  for (n in seq(-1, 1, .01)){ abline(h = n, col = "grey", lty = 3) }
 }
-# Test
-qq.plt(returns_df)
+qq.plt(returns_df) # Test
