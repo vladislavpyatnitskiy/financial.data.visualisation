d5f97ea7748acc1c003997ba0a4af0057925b650
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-11-04 15:08:16 +0500

    Update Histogram for VaR.R
    Reduced excessive lines

--- Histogram/Histogram for VaR.R
+++ Histogram/Histogram for VaR.R
@@ -1,30 +1,21 @@
-# Calculate VaR via Historical Method
-histVaR.plt <- function(x, VaR, lg = T){
+library(fBasics)
+
+histVaR.plt <- function(x, VaR, lg = T){ # VaR by Historical Method
   
-  # Check whether there are less than 100 observations
-  if (nrow(x) < 100) { 
-    print("Error. Insufficient number of observations for analysis.") } else {
+  if (nrow(x)<100) { # Check whether there are less than 100 observations
+    print("Error. Insufficient number of observations.") } else {
       
-      # Calculate log returns and remove NA if necessary
-      if (isTRUE(lg)) { x <- diff(log(x))[-1,] }
+      if (isTRUE(lg)) { x <- diff(log(x))[-1,] } # Log returns & NA off
       
       # Calculate historical VaR value and transform into matrix format
-      VaR_value <- as.matrix(apply(x, 2,
-                           function(col) quantile(col, 1 - VaR * 0.01)))
-      # For each column
-      for (n in 1:ncol(x)){
+      VaR_value <- as.matrix(apply(x,2,function(col) quantile(col, 1-VaR*.01)))
+      
+      # For each column make histogram
+      for (n in 1:ncol(x)){ histPlot(x[,n], las = 1) 
         
-        # Make histogram
-        histPlot(x[,n], las = 1)
+        abline(v = VaR_value[n], lwd = 3, col = "red") # line for VaR Value
         
-        # Add line to show VaR Value
-        abline(v = VaR_value[n], lwd = 3, col = "red")
-        
-        # Add text
-        text(x = VaR_value[n] + 0.75 * VaR_value[n],
-             y = 7.5, sprintf("VaR %s is %s", VaR, round(VaR_value[n],2)))
-        }
-    }
+        text(x=VaR_value[n]+.75*VaR_value[n],y=7.5, # Add text
+             sprintf("VaR %s is %s", VaR, round(VaR_value[n],2))) } }
 }
-# Test
-histVaR.plt(stock_data, 95)
+histVaR.plt(stock_data, 95) # Test
