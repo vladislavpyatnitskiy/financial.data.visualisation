e2ba96769da4915162d75e4ba53937f868c860e6
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-11-04 19:26:45 +0500

    Update Alternative script for Dual Plot.R
    Optimised script:
    
    • Reduced excessive lines
    • Combined operations
    • Edited comments
    • Renamed variables

--- Dual Axis Chart/Alternative script for Dual Plot.R
+++ Dual Axis Chart/Alternative script for Dual Plot.R
@@ -1,40 +1,22 @@
-# library to use
-library(latticeExtra)
+library(latticeExtra) # library to use
 
-# Function 
-dual_axis_plot <- function(x){
+# Dual Plot
+dual.plt <- function(x){ d <- 1:nrow(x) # Set up array for x-axis
   
-  # Set up array for x-axis
-  dates_for_dual_plot <- 1:nrow(x) 
+  f <- as.matrix(x[,1]) # Set up array for first y-axis
   
-  # Set up array for first y-axis
-  first_dual_part <- as.matrix(x[,1])
+  s <- as.matrix(x[,2]) # Set up array for second y-axis
   
-  # Set up array for second y-axis
-  second_dual_part <- as.matrix(x[,2])
-  
-  # Add all of them into one df
-  data_for_dual_plot <- data.frame(dates_for_dual_plot,
-                                   first_dual_part,
-                                   second_dual_part)
+  data <- data.frame(d, f, s) # Add all of them into one df
   
   # First data to plot
-  obj1 <- xyplot(first_dual_part ~ dates_for_dual_plot,
-                 data_for_dual_plot,
-                 ylab = colnames(first_dual_part),
-                 xlab = "Trading Days",
-                 main = "Stock Performance Comparison",
-                 type = "l" , lwd=2, col="steelblue",
-                 sub = "Source: Yahoo! Finance")
+  obj1 <- xyplot(f ~ d,data,ylab=colnames(f),xlab="Trading Days", type="l",
+                 main="Stock Performance Comparison", lwd=2, col="steelblue",
+                 sub = "Data Source: Yahoo! Finance")
   
   # Second data to plot
-  obj2 <- xyplot(second_dual_part ~ dates_for_dual_plot,
-                 data_for_dual_plot,
-                 ylab = colnames(second_dual_part),
-                 type = "l", lwd=2, col="#69b3a2")
+  obj2<-xyplot(s ~ d,data,ylab=colnames(s),type="l",lwd=2,col="#69b3a2")
   
-  # Make the plot with second y axis:
-  doubleYScale(obj1, obj2, add.ylab2 = TRUE)
+  doubleYScale(obj1, obj2, add.ylab2 = T) # Make the plot with second y axis:
 }
-# Test 
-dual_axis_plot(portfolioReturns)
+dual.plt(portfolioReturns) # Test 
