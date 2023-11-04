7b64c8a49934720ded38f17c6c2d886c9bb13c6e
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-11-04 15:29:29 +0500

    Update Bond Plot.R
    Reduced excessive lines

--- Line Plot/Bond Plot.R
+++ Line Plot/Bond Plot.R
@@ -1,11 +1,10 @@
 # Relationship between rate and price
-bond.plt <- function(P, C = 0, ytm, f = 1, l.bound = 0, h.bound = 100){
+bond.y.plt <- function(P, C = 0, y, f = 1, l.bound = 0, h.bound = 100){
   
-  # Empty lists for prices
-  l.B.prices <- NULL
+  l.B.prices <- NULL # Empty lists for prices
   
   # For each yield value add bond price value to list
-  for (n in l.bound:h.bound){ d <- (1 + .01 * n / f) ^ ytm * f
+  for (n in l.bound:h.bound){ d <- (1 + .01 * n / f) ^ y * f
   
   l.B.prices <- c(l.B.prices, P * (C / .01 / n * (1 - 1 / d) + 1 / d)) }
   
@@ -25,8 +24,7 @@ bond.plt <- function(P, C = 0, ytm, f = 1, l.bound = 0, h.bound = 100){
   axis(side = 2, at = seq(200, 1200, 100), las = 1)
   
   # Dotted lines
-  abline(v = seq(-1, 1, .1), lty = 3, col = "grey")
+  abline(v = seq(-1, 1, .1), lty = 3, col = "grey") 
   abline(h = seq(0, 1500, 100), lty = 3, col = "grey")
 }
-# Test
-bond.plt(P = 1000, C = .08, ytm = 3, f = 1, l.bound = 0, h.bound = 100)
+bond.y.plt(P = 1000, C = .08, y = 3, f = 1, l.bound = 0, h.bound = 100) # Test
