f220d7fef7dcf37d6578fcf29845bf02b8e241e2
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-11-04 15:55:44 +0500

    Update Pie Chart.R
    Optimisation: Reduced excessive lines & Combined operations

--- Pie Chart/Pie Chart.R
+++ Pie Chart/Pie Chart.R
@@ -1,23 +1,11 @@
 # Function to facilitate Pie Plot generation
-piePlot <- function(slices, tickers, main = NULL, sub = NULL){
+pie.plt <- function(slices, tickers, main = NULL, sub = NULL){
   
-  # Percentage calculation
-  pct <- round(slices / sum(slices) * 100)
-  
-  # Join tickers with percentages
-  lbls <- sprintf("%s %s%%", tickers, pct)
-  
-  # Pie Chart configuration
-  pie(slices,
-      labels = lbls,
-      col = rainbow(length(lbls)),
-      main = main,
-      sub = sub) 
+  pie(slices, col = rainbow(length(lbls)), main = main, sub = sub,
+      labels = sprintf("%s %s%%", tickers, round(slices / sum(slices) * 100))) 
 }
-
 # Test
-piePlot(slices = c(0.278716291651169, 0.411679570881108, 0.0727978571595576,
+pie.plt(slices = c(0.278716291651169, 0.411679570881108, 0.0727978571595576,
                    0.176817615423501, 0.0599886648846643),
         tickers = c("BELU", "BSPB", "IRAO", "LKOH", "RTKM"),
-        main = "Dividend Yield Distribution",
-        sub = "Source: Investing.com")
+        main="Dividend Yield Distribution", sub="Data Source: Investing.com")
