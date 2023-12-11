d7cc4b8c163167bd1f5372c4df8bc30ac6cbc008
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-12-11 15:16:12 +0500

    Update Scatter Plot for Risk & Return.R
    Optimised script:
    
    • Combined operations of formatting data into time series, calculating logs, expected return and standard deviation into one apply function
    • Rewrote some of the variables as there is no longer need for separate variable name for standard deviation values
    • Reduced excessive lines
    • Edited comments

--- Scatter Plot/Scatter Plot for Risk & Return.R
+++ Scatter Plot/Scatter Plot for Risk & Return.R
@@ -4,45 +4,38 @@ lapply(c("quantmod", "timeSeries"), require, character.only = T) # Libraries
 risk.return.plt <- function(x,s = NULL,e = as.Date(Sys.Date()),data = T,lg=F){
   
   if (isTRUE(data)){ p <- NULL # Empty variable for values when data is needed
+  
+  for (A in x){ # For each ticker get data
+    p<-cbind(p,getSymbols(A,from=s,to=e,src="yahoo",auto.assign=F)[,4])}
+  
+  p <- p[apply(p,1,function(x) all(!is.na(x))),] # Get rid of NA
+  
+  colnames(p) <- x # Give column names 
+  
+  r<-apply(diff(log(as.timeSeries(p)))[-1,], 2, # time series & log & AVG & SD
+           function(col) c((exp(sum(col)) - 1) * 100, sd(col) * 1000)) 
+  
+  plot(r[2,], r[1,], xlab = "Risk (Standard Deviation)", ylab = "Return (%)",
+       main = "Risk & Return Plot", ylim = c(min(r[1,]) - 3, max(r[1,]) + 3),
+       xlim = c(min(r[2,]) - 3, max(r[2,]) + 3), las = 1) # Plot
+  
+  abline(lm(r[1,] ~ r[2,]), col = "red", lwd = 3)
+  abline(h = 0) # Add regression & horizontal line
+  
+  text(r[2,], r[1,] + 1, labels = names(r[2,]), pos = 4) # Put labels
+  
+  } else { if (isTRUE(lg)){ p <- diff(log(as.timeSeries(x)))[-1,] }
     
-    for (A in x){ # For each ticker get data
-      p<-cbind(p,getSymbols(A,from=s,to=e,src="yahoo",auto.assign=F)[,4])}
+    r<-apply(p,2,function(col) c((exp(sum(col))-1)*100,sd(col)*1000)) #AVG & SD
     
-    p <- p[apply(p,1,function(x) all(!is.na(x))),] # Get rid of NA
+    plot(r[2,], r[1,], xlab = "Risk (Standard Deviation)", ylab = "Return (%)",
+         main = "Risk & Return Plot", ylim = c(min(r[1,]) - 3, max(r[1,]) + 3),
+         xlim = c(min(r[2,]) - 3, max(r[2,]) + 3), las = 1) # Plot
     
-    colnames(p) <- x # Give column names 
+    abline(lm(r[1,] ~ r[2,]), col = "red", lwd = 3)
+    abline(h = 0) # Add regression & horizontal line
     
-    p <- diff(log(as.timeSeries(p)))[-1,] # Calculate logs
-    
-    r <- apply(p,2,function(col) ((exp(cumsum(col))-1)*100)[nrow(p)]) # Return
-    
-    r.sd <- apply(p, 2, function(col) sd(col) * 1000) # Standard Deviation
-    
-    plot(r.sd, r, las = 1, xlab="Risk (Standard Deviation)",ylab="Return (%)",
-         main = "Risk & Return Plot", ylim = c(min(r)-3, max(r)+3),
-         xlim = c(min(r.sd)-3, max(r.sd)+3)) # Plot
-    
-    abline(lm(r ~ r.sd), col="red", lwd=3) # Add regression line
-    
-    abline(h = 0) # Add horizontal line 
-    
-    text(r.sd, r+1, labels=names(r.sd), pos = 4) # Put labels
-    
-  } else { if (isTRUE(lg)){ p <-diff(log(as.timeSeries(x)))[-1,] }
-      
-    r <- apply(p,2,function(col) ((exp(cumsum(col))-1)*100)[nrow(p)]) # Return
-    
-    r.sd <- apply(p, 2, function(col) sd(col) * 1000) # Standard Deviation
-    
-    plot(r.sd, r, las = 1, xlab="Risk (Standard Deviation)",ylab="Return (%)",
-         main = "Risk & Return Plot", ylim = c(min(r)-3, max(r)+3),
-         xlim = c(min(r.sd)-3, max(r.sd)+3)) # Plot
-    
-    abline(lm(r ~ r.sd), col="red", lwd=3) # Add regression line
-    
-    abline(h = 0) # Add horizontal line 
-    
-    text(r.sd, r+1, labels=names(r.sd), pos = 4)} # Put labels
+    text(r[2,], r[1,]+1, labels=names(r[2,]), pos = 4) } # Put labels
 }
 risk.return.plt(x = c("AMZN", "GOOGL", "MSFT", "AAPL", "NFLX", "META", "NVDA",
                       "TSLA", "ORCL", "INTC", "TSM", "ASML", "AMD", "QCOM",
