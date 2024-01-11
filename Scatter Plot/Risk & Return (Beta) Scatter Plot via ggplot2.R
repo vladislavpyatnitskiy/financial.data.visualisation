410db1da010325ab973cdb97e1f8331f6363c995
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-01-11 19:21:52 +0500

    Update Risk & Return (Beta) Scatter Plot via ggplot2.R
    Added option to generate plot with third party data

--- Scatter Plot/Risk & Return (Beta) Scatter Plot via ggplot2.R
+++ Scatter Plot/Risk & Return (Beta) Scatter Plot via ggplot2.R
@@ -3,20 +3,32 @@ lapply(c("quantmod","timeSeries","ggplot2","ggrepel"),require,character.only=T)
 
 # Scatter Plot for selected securities with Beta via ggplot2
 scatter.plt.beta <- function(x, s = NULL,e = as.Date(Sys.Date()), i = "^GSPC",
-                            xlab = NULL, ylab = NULL, main = NULL){
+                             xlab = NULL, ylab = NULL, main = NULL, data = T,
+                             lg = T){
   
-  x <- c(x, i) # Add S&P 500 ticker to rest
-  
-  p <- NULL # Empty variable and get data from Yahoo! Finance
-  
-  for (A in x){ p <- cbind(p, getSymbols(A, from = s, to = e, src = "yahoo",
-                                         auto.assign = F)[,4]) }
-  
-  p <- p[apply(p, 1, function(x) all(!is.na(x))),] # Get rid of NA
-  
-  colnames(p) <- x # Give column names 
-  
-  p <- diff(log(as.timeSeries(p)))[-1,] # Calculate logs
+  if (isTRUE(data)){ x <- c(x, i) # Add S&P 500 ticker to rest
+    
+    p <- NULL # Empty variable and get data from Yahoo! Finance
+    
+    for (A in x){ p <- cbind(p, getSymbols(A, from = s, to = e, src = "yahoo",
+                                           auto.assign = F)[,4]) }
+    
+    p <- p[apply(p, 1, function(x) all(!is.na(x))),] # Get rid of NA
+    
+    colnames(p) <- x # tickers
+    
+    x <- p } else { # When no data is needed
+      
+      c <- getSymbols(i, from = rownames(x)[1], to = rownames(x)[nrow(x)], 
+                      src = "yahoo", auto.assign = F)[,4]
+      
+      c <- c[apply(c, 1, function(x) all(!is.na(x))),] # Reduce NA
+      
+      colnames(c) <- i # tickers
+      
+      x <- cbind(x, c) } # Join with ready data
+    
+  if (isTRUE(lg) || isTRUE(data)) { p <- diff(log(as.timeSeries(x)))[-1,] }
   
   o <- t(apply(p[,-which(names(p) == i)], 2,
                function(col) c(((exp(sum(col)) - 1) * 100),
