6bd6701a56fbd06e10355506b19d7c62e0bb1f4d
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-07-12 11:59:06 +0500

    Update Boxplot.R
    Updated and optimised script:
    
    • Improved lines generation
    • Added right y-axis

--- Boxplot/Boxplot.R
+++ Boxplot/Boxplot.R
@@ -1,25 +1,24 @@
 lapply(c("quantmod", "timeSeries"), require, character.only = T) # Libraries
 
-# Function to create boxplot
-box.plt <- function(x, s = NULL, e = NULL, main = NULL, data = T, lg = F){
+box.plt <- function(x, s = NULL, e = NULL, data = T, lg = F){ # Box Plot
   
   if (isTRUE(data)){ p <- NULL # data off
   
-  for (A in x){ if (is.null(s) && is.null(e)) { # When dates are not defined
+    for (A in x){ if (is.null(s) && is.null(e)) { # When dates are not defined
     
-    p <- cbind(p, getSymbols(A, src = "yahoo", auto.assign = F)[,4])
+        p <- cbind(p, getSymbols(A, src = "yahoo", auto.assign = F)[,4])
     
-  } else if (is.null(e)) { # When only start date is defined
+    } else if (is.null(e)) { # When only start date is defined
     
-    p <- cbind(p, getSymbols(A, from = s, src="yahoo", auto.assign = F)[,4])
+      p <- cbind(p, getSymbols(A, from = s, src="yahoo", auto.assign = F)[,4])
     
-  } else if (is.null(s)) { # When only end date is defined
+    } else if (is.null(s)) { # When only end date is defined
     
-    p <- cbind(p, getSymbols(A, to = e, src = "yahoo", auto.assign = F)[,4])
+      p <- cbind(p, getSymbols(A, to = e, src = "yahoo", auto.assign = F)[,4])
     
-  } else { # When both start date and end date are defined
+    } else { # When both start date and end date are defined
     
-    p <- cbind(p,getSymbols(A,from=s,to=e,src="yahoo",auto.assign=F)[,4]) } }
+      p <- cbind(p,getSymbols(A,from=s,to=e,src="yahoo",auto.assign=F)[,4]) } }
   
   p <- p[apply(p, 1, function(x) all(!is.na(x))),] # Get rid of NA
   
@@ -29,11 +28,26 @@ box.plt <- function(x, s = NULL, e = NULL, main = NULL, data = T, lg = F){
   
   if (isTRUE(lg) || isTRUE(data)) { x <- diff(log(as.timeSeries(x)))[-1,] }
   
-  boxplot.matrix(x, main = main, title = F, las = 1, col = "steelblue",
-                 xlab = "Data Source: Yahoo Finance", ylab = "Returns")
+  boxplot.matrix(x, main = "Fluctuations of Securities", title = F, las = 1,
+                 col = "steelblue", xlab = "Data Source: Yahoo Finance")
   
-  abline(h = 0, col = "grey", lty = 3) # data is ready
+  l <- as.numeric(x)[!is.na(as.numeric(x))] # Get values & define intervals
+  
+  m <- round(min(l) * -1 + max(l),1)/10^(nchar(round(min(l) * -1 + max(l),1)))
+  
+  if (m > 0 && m < 1){ mn <- 1 * 10 ^ (nchar(m) - 6) }
+  
+  else if (m > 1 && m < 2){ mn <- 2 * 10 ^ (nchar(m) - 6) }
+  
+  else if (m > 2 && m < 5){ mn <- 5 * 10 ^ (nchar(m) - 6) }
+  
+  axis(side = 4, at = seq(-1, 1, by = mn / 2), las = 2)
+  
+  par(mar = c(5, 4, 4, 4)) # Define borders of the plot to fit right y-axis
+  
+  # Add Vertical and Horizontal lines
+  abline(h=seq(-1,1,by=mn/2)[-match(0, seq(-1,1,by=mn/2))], col="grey", lty=3)
+  abline(v = seq(ncol(x)), col = "grey", lty = 3) 
+  abline(h = 0, col = "black", lty = 3) 
 }
-# Test
-box.plt(c("AAPL", "MSFT", "META", "GOOGL", "AMZN"),s = "2023-01-01",
-        main = "Boxplot of IT companies")
+box.plt(c("AAPL", "MSFT", "META", "GOOGL", "AMZN"), s = "2023-01-01") # Test
