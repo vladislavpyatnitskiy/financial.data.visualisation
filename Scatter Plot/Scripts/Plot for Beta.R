946e8a8bd0115bdb1b74877757480d354b362d49
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-09-25 21:04:50 +0500

    Update Plot for Beta.R
    Optimised script:
    
    • Rewrote FOR loop and IF statements for data extraction

--- Scatter Plot/Scripts/Plot for Beta.R
+++ Scatter Plot/Scripts/Plot for Beta.R
@@ -1,28 +1,23 @@
 lapply(c("quantmod", "timeSeries"), require,character.only = T) # Libraries
 
-# Function to visualise Beta Scatter Plot
-beta.plt <- function(y, i = "^GSPC", s = NULL, e = NULL){
+beta.plt <- function(y, i = "^GSPC", s=NULL, e=NULL){ # Scatter Plot of Beta
   
   y <- c(y, i) # Add benchmark to list
   
   p <- NULL # Create an empty variable
   
-  for (A in y){ if (is.null(s) && is.null(e)) { # Download data when dates off
+  for (A in y){ if (is.null(s) && is.null(e)) { # 4 scenarios
     
-      p <- cbind(p, getSymbols(A, src = "yahoo", auto.assign = F)[,4])
+      q <- getSymbols(A, src = "yahoo", auto.assign = F)
+      
+    } else if (is.null(e)){ q<-getSymbols(A,from=s,src="yahoo",auto.assign=F)
     
-    } else if (is.null(e)) { # When only start date is defined
+    } else if (is.null(s)){ q<-getSymbols(A,to=e,src="yahoo",auto.assign=F)
     
-      p <- cbind(p,getSymbols(A, from = s, src = "yahoo", auto.assign = F)[,4])
+    } else { q <- getSymbols(A,from=s,to=e,src="yahoo",auto.assign=F) }
+      
+    p <- cbind(p, q[,4]) } # Join all columns into one data frame
     
-    } else if (is.null(s)) { # When only end date is defined
-    
-      p <- cbind(p, getSymbols(A, to = e, src = "yahoo", auto.assign = F)[,4])
-    
-    } else { # When both start date and end date are defined
-    
-      p <- cbind(p,getSymbols(A,from=s,to=e,src="yahoo",auto.assign=F)[,4]) } }
-  
   p <- p[apply(p, 1, function(x) all(!is.na(x))),] # Get rid of NA
   
   colnames(p) <- y # Put the tickers in data set
@@ -31,13 +26,11 @@ beta.plt <- function(y, i = "^GSPC", s = NULL, e = NULL){
   
   R <- x[,-which(names(x) == i)] # Subset index from data set
   
-  # Loop generates Scatter plots with trend line  
-  for (n in 1:ncol(R)) { S <- R[,n]
+  for (n in 1:ncol(R)) { S <- R[,n] # Scatter plot for each stock
   
-    plot(x[,i], S, las = 1, ylab = sprintf("%s Return (%%)", colnames(S)),
-         main = sprintf("%s Beta", colnames(S)), xlab = "Market Return (%)",
-         sub = "Data Source: Yahoo Finance")
-    
-    abline(lm(S ~ x[,i]), col = "red", lwd = 3) }
+    plot(x[,i], S, sub = "Data Source: Yahoo Finance",  xlab = "Market Return",
+         las = 1, ylab = "Stock Return", main=sprintf("%s Beta", colnames(S)))
+  
+    abline(lm(S ~ x[,i]), col = "red", lwd = 3) } # Regression line
 }
 beta.plt(c("AIG", "MET", "UNM", "HIG", "OMF"), i = "^GSPC") # Test
