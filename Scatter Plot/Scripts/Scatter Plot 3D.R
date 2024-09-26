3b5836645cfcba8d1ae8d584800322cdd0f69959
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-09-26 11:23:45 +0500

    Update Scatter Plot 3D.R
    Optimised script:
    
    • Rewrote FOR loop and IF statements for data extraction
    • Reduced excessive lines

--- Scatter Plot/Scripts/Scatter Plot 3D.R
+++ Scatter Plot/Scripts/Scatter Plot 3D.R
@@ -1,41 +1,30 @@
-lapply(c("quantmod","timeSeries","plotly"),require,
-       character.only=T) # libraries
+lapply(c("quantmod", "timeSeries", "plotly"), require, character.only=T) # libs
 
-scatter.plt.3d <- function(y, s = NULL, e = NULL, zaxis = NULL, yaxis = NULL,
-                           xaxis = NULL){
+scatter.plt.3d <- function(y,s=NULL,e=NULL,zaxis=NULL,yaxis=NULL,xaxis=NULL){
   
-  p <- NULL # Create an empty variable
+  p <- NULL # 4 scenarios for data extraction 
   
-  # Loop for data extraction & # Set up statements for start and end dates
-  for (Ticker in y){ if (is.null(s) && is.null(e)) {
+  for (A in y){ if (is.null(s) && is.null(e)) {
     
-      # When neither start date nor end date are defined
-      p <- cbind(p, getSymbols(Ticker, src = "yahoo", auto.assign=F)[,4])
+      q <- getSymbols(A, src = "yahoo", auto.assign = F)
       
-    } else if (is.null(e)) { # When only start date is defined
+      } else if (is.null(e)){ q<-getSymbols(A,from=s,src="yahoo",auto.assign=F)
       
-      p <- cbind(p, getSymbols(Ticker, from = s,src="yahoo",auto.assign=F)[,4])
+      } else if (is.null(s)){ q<-getSymbols(A,to=e,src="yahoo",auto.assign=F)
       
-    } else if (is.null(s)) { # When only end date is defined
-      
-      p <- cbind(p,getSymbols(Ticker,to=e,src="yahoo",auto.assign=F)[,4])
-      
-    } else { # When both start date and end date are defined
-      
-      p<-cbind(p,getSymbols(Ticker,from=s,to=e,src="yahoo",auto.assign=F)[,4])}
-    }
+      } else { q <- getSymbols(A, from=s, to=e, src="yahoo", auto.assign=F) }
+        
+    p <- cbind(p, q[,4]) } # Join all columns into one data frame
+    
   p <- p[apply(p, 1, function(x) all(!is.na(x))),] # Get rid of NA
   
   colnames(p) <- y # Put the tickers in data set
   
   r <- as.data.frame(p) # Make it time series
   
-  fig <- plot_ly(r, x = ~r[,3], y = ~r[,2], z = ~r[,1])
-  fig <- fig %>% add_markers()
-  fig <- fig %>% layout(scene = list(xaxis = list(title = xaxis),
-                                     yaxis = list(title = yaxis),
-                                     zaxis = list(title = zaxis)))
-  fig # Display
+  plot_ly(r, x = ~r[,3], y = ~r[,2], z = ~r[,1]) %>% add_markers() %>%
+    layout(scene = list(xaxis = list(title=xaxis), yaxis = list(title=yaxis),
+                        zaxis = list(title=zaxis)))
 }
-scatter.plt.3d(y = c("UNM", "CL=F", "GC=F"), s = "2022-01-01", zaxis = "UNM",
+scatter.plt.3d(y = c("UNM", "CL=F", "GC=F"), s = "2022-01-01", zaxis = "XOM",
                yaxis = "Oil", xaxis = "Gold")
