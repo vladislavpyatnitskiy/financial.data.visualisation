5ce9457c697b85793c3e2c216d8667a3c8ad12b0
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-04-15 11:02:11 +0500

    Update Plot with MA.R
    Rewrote part for data extraction

--- Line Plot/Scripts/Plot with MA.R
+++ Line Plot/Scripts/Plot with MA.R
@@ -4,28 +4,26 @@ ma.plt <- function(x, s = NULL, e = NULL, data = T){ # Plot of Moving Averages
   
   if (isTRUE(data)){ p <- NULL # Data download loop for 4 scenarios:
   
-    for (A in x){ if (is.null(s) && is.null(e)) { 
-      
-        q <- getSymbols(A, src = "yahoo", auto.assign = F)
-      
-      } else if (is.null(e)){ q<-getSymbols(A,from=s,src="yahoo",auto.assign=F)
+    src <- "yahoo"
     
-      } else if (is.null(s)){ q<-getSymbols(A,to=e,src="yahoo",auto.assign=F)
+    getData <- function(A, s, e) {
+      if (is.null(s) && is.null(e)) return(getSymbols(A,src=src,auto.assign=F)) 
+      if (is.null(e)) return(getSymbols(A, from = s, src=src, auto.assign=F)) 
+      if (is.null(s)) return(getSymbols(A, to = e, src=src, auto.assign=F)) 
+      return(getSymbols(A, from = s, to = e, src=src, auto.assign=F)) 
+    }
+    for (A in x){ p <- cbind(p, getData(A, s, e)[,4]) } # Join data
     
-      } else { q <- getSymbols(A,from=s,to=e,src="yahoo",auto.assign=F) }
-      
-      p <- cbind(p, q[,4]) } # Join all columns into one data frame
+    p <- p[apply(p, 1, function(x) all(!is.na(x))),] # Remove NA
     
-      p <- p[apply(p, 1, function(x) all(!is.na(x))),] # Remove NA
+    colnames(p) <- x
     
-      colnames(p) <- x
-      
-      x <- p } # Give tickers to data
+    x <- p } # Give tickers to data
     
   r <- as.timeSeries(x) # Make data time series
   
   for (n in 1:ncol(r)){
-  
+    
     chartSeries(round(r[,n], 2), theme = "white",
                 name = sprintf("%s Stock Performance", colnames(r[,n])),
                 TA = "addEMA(50, col='purple');addEMA(200, col='red')") }
