0d87d7d0f4e8484cc02a964228d656b3c3440972
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-04-16 13:18:43 +0500

    Update Volatility Plot.R
    Optimised script:
    
    • Rewrote script entirely
    • Reduced excessive lines
    • Improved grid

--- Line Plot/Scripts/Volatility Plot.R
+++ Line Plot/Scripts/Volatility Plot.R
@@ -1,43 +1,18 @@
 volatility.plt <- function(x, abs = F){ # Plot Security volatility
   
-  x <- diff(log(x)) # Calculate logs
-  x[1,] <- 0 # Assign first value as 0
+  x <- diff(log(x))[-1,] * 100 # Calculate logs
+  main = ifelse(abs == T, "Volatility", "Fluctuations")
+  if (isTRUE(abs)){ x <- abs(x) }
   
-  x <- x * 100 # Multiply by 100
+  for (n in 1:ncol(x)){ 
+    
+    plot(x[,n], col = "red", ylab = "Returns (%)", xlab = "Trading Days",
+         main = sprintf("%s %s", colnames(x[,n]), main), las = 2,
+         ylim = c(min(x[,n]), max(x[,n]))) 
   
-  if (isFALSE(abs)){ # Choose between real and absolute values of returns
-  
-    for (n in 1:ncol(x)){ # Plot volatility with positive and negative returns
-        
-      plot(x[,n], col = "red", ylab = "Returns (%)", xlab = "Trading Days",
-           main = sprintf("%s Volatility", colnames(x[,n])), las = 2,
-           ylim = c(min(x[,n]), max(x[,n])))
-      
-      axis(side = 2, at = seq(-100, 100, 5), las = 2) # Set up axis
-        
-      abline(h = 0)
-      abline(h = seq(-100, -5, 5), col = "grey", lty = 3)
-      abline(h = seq(5, 100, 5), col = "grey", lty = 3) } } else {
-        
-        x <- abs(x)
-          
-    for (n in 1:ncol(x)){ # Plot returns fluctuations
-        
-      plot(x[,n], col = "red", ylab = "Fluctuations (%)", las = 2,
-           xlab = "Trading Days", ylim = c(min(x[,n]), max(x[,n])),
-           main = sprintf("%s Volatility", colnames(x[,n])))
-      
-      if (max(x[,n]) < 15){
-        
-        axis(side = 2, at = seq(-100, 100, 2), las = 2) # Set up axis
-        
-        abline(h = 0)
-        abline(h = seq(-100, -2, 2), col = "grey", lty = 3)
-        abline(h = seq(2, 100, 2), col = "grey", lty = 3) } else {
-      
-      axis(side = 2, at=seq(100, from = 5, by = 5), las = 2) # Set up axis
-          
-      abline(h = 0)
-      abline(h = seq(5, 100, 5), col = "grey", lty = 3) } } }
+    axis(side = 4, las = 2) # Right y axis and lines
+    
+    grid(nx = 1, ny = NULL, col = "grey", lty = "dotted", lwd = 1)
+    abline(h = 0) }
 }
-volatility.plt(stock_data, abs = F)
+volatility.plt(stock_data, abs = T)
