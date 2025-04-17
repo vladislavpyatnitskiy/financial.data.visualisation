39161fad21edd0117cc24fc63409432e7af7a0f7
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-04-17 11:29:56 +0500

    Update QQ plot.R
    Optimised script:
    
    • Rewrote part for data extraction
    • Reduced excessive lines

--- QQ Plot/QQ plot.R
+++ QQ Plot/QQ plot.R
@@ -1,42 +1,37 @@
-# Function to generate Q-Q plot
-qq.plt <- function(x, lg = F, data = F, s = NULL, e = NULL){ 
+lapply(c("timeSeries", "quantmod"), require, character.only = T) # Libs
+
+qq.plt <- function(x, lg = T, data = T, s = NULL, e = NULL){ # Q-Q plot
   
-  if (isTRUE(data) && (isTRUE(lg) || isFALSE(lg))){ p <- NULL # data off
+  if (isTRUE(data)){ p <- NULL 
   
-  for (A in x){ if (is.null(s) && is.null(e)) { # When dates are not defined
+    src <- "yahoo" # Source
     
-    p <- cbind(p, getSymbols(A, src = "yahoo", auto.assign = F)[,4])
+    getData <- function(A, s, e) {
+      if (is.null(s) && is.null(e)) return(getSymbols(A,src=src,auto.assign=F)) 
+      if (is.null(e)) return(getSymbols(A, from = s, src=src, auto.assign=F)) 
+      if (is.null(s)) return(getSymbols(A, to = e, src=src, auto.assign=F)) 
+      return(getSymbols(A, from = s, to = e, src=src, auto.assign=F)) 
+    }
+    for (A in x){ p <- cbind(p, getData(A, s, e)[,4]) } # Join data
     
-  } else if (is.null(e)) { # When only start date is defined
+    p <- p[apply(p, 1, function(x) all(!is.na(x))),] # Get rid of NA
     
-    p <- cbind(p, getSymbols(A, from = s, src="yahoo", auto.assign=F)[,4])
+    colnames(p) <- x # Put tickers as column names
     
-  } else if (is.null(s)) { # When only end date is defined
+    x <- p } # Make it time series, log and NA off 
     
-    p <- cbind(p, getSymbols(A, to = e, src="yahoo", auto.assign = F)[,4])
+  if (isTRUE(lg)) { x = diff(log(as.timeSeries(x)))[-1,] } # data on
     
-  } else { # When both start date and end date are defined
-    
-    p <- cbind(p,getSymbols(A,from=s,to=e,src="yahoo",auto.assign=F)[,4]) } }
-  
-  p <- p[apply(p, 1, function(x) all(!is.na(x))),] # Get rid of NA
-  
-  colnames(p) <- x # Put tickers as column names
-  
-  x = diff(log(as.timeSeries(p)))[-1,] # Make it time series, log and NA off 
-  
-  } else if (isFALSE(data) && isTRUE(lg)) { x = diff(log(x))[-1, ] } # data on
-  
   for (n in 1:ncol(x)){ s <- x[,n] # For each asset make qq plot
-  
-    qqnorm(s, main = sprintf("%s Q-Q Plot", colnames(s)), las = 1) # QQ plot
     
-    for (n in 1:2){ axis(side = 2 * n, at = seq(-1,1,.01), las = 1) } # Y-axes
+    qqnorm(s, main = sprintf("%s Q-Q Plot", colnames(s)), las = 1) # QQ plot
+      
+    axis(side = 4, las = 2) # Right y-axis
     
     qqline(s, col = "red", lwd = 3)  # Add line
     
-    # Add grey dotted vertical & horizontal lines
-    for (n in seq(-3, 3, 1)){ abline(v = n, col = "grey", lty = 3) }
-    for (n in seq(-1, 1, .01)){ abline(h = n, col = "grey", lty = 3) } }
+    grid(nx = NULL, ny = NULL, col = "grey", lty = "dotted", lwd = 1) 
+    
+    par(mar = rep(5, 4)) } # Margins
 }
 qq.plt(x = c("AIG", "OMF", "UNM"), lg = T, data = T ,s = "2023-10-01") # Test
