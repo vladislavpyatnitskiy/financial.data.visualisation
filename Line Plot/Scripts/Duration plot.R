09bc1f56aeb0f7e3dacd65000143f3383c22d12c
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-04-21 11:47:33 +0500

    Update Duration plot.R
    Updated script:
    
    • Added Right y-axis
    • Added Margins
    • Rewrote part for grid

--- Line Plot/Scripts/Duration plot.R
+++ Line Plot/Scripts/Duration plot.R
@@ -1,18 +1,18 @@
 # Function to show Duration dependency on Year to Maturity 
-duration.plt.ytm <- function(P, C, r, f = 1, ytm = 100, D = NULL){
+duration.plt.ytm <- function(P, C, r, f = 1, ytm = 100){ D = NULL
   
-  for (y in seq(ytm,from = 1)){ P. <- P * (1 + C/f)/(1 + r/f)^(y*f) # Principle
+  for (y in seq(ytm,from = 1)){ Pr <- P * (1 + C/f)/(1 + r/f)^(y*f) # Principle
   
     PV <- NULL # Variable to store PV
-    payments <- NULL # Variable to store payments
+    pays <- NULL # Variable to store payments
     
     for (n in 1:(y * f - 1)){ PV <- cbind(PV, C * P / f / (1 + r / f) ^ n * f) 
     
-      payments <- cbind(payments, n * PV[n]) } # Coupon PV
-      
-    # Duration
-    D <- rbind(D,(sum(payments[seq(y*f-1)])+P.*y*f)/(P.+sum(PV[seq(y*f-1)])))}
+      pays <- cbind(pays, n * PV[n]) } # Coupon PV
     
+    # Duration
+    D <- rbind(D, (sum(pays[seq(y*f-1)])+Pr*y*f) / (Pr+sum(PV[seq(y*f-1)]))) }
+  
   # Generate plot
   plot(x = seq(ytm, from = 1),
        y = D,
@@ -20,15 +20,14 @@ duration.plt.ytm <- function(P, C, r, f = 1, ytm = 100, D = NULL){
        xlab = "Bond Year to Maturity",
        ylab = "Bond Duration",
        main = "Bond Duration Dependency On Year to Maturity",
-       sub = "Source: None",
        col = "red",
        lwd = 3,
        las = 1)
   
-  axis(side = 1, at=seq(10, 100, 10)) # Axes
-  axis(side = 2, at=seq(0, 100, 1), las = 1)
+  axis(side = 4, las = 2)
   
-  abline(v = seq(0, 100, 10), lty = 3, col = "grey") # lines 
-  abline(h = seq(0, 100, 1), lty = 3, col = "grey")
+  grid(nx = NULL, ny = NULL, lty = 3, col = "grey") # Horizontal lines
+  
+  par(mar = c(5, 5, 5, 4)) # Define borders of the plot
 }
 duration.plt.ytm(1000, 0.01, 0.1) # Test
