e16e3081c79950c82bd8686d4f8844d418322891
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2026-01-23 11:21:54 +0500

    Refactor box.plt function for better clarity

--- Boxplot/Boxplot.R
+++ Boxplot/Boxplot.R
@@ -2,8 +2,8 @@ lapply(c("quantmod", "timeSeries"), require, character.only = T) # Libraries
 
 box.plt <- function(x, s = NULL, e = NULL, data = T, lg = F){ # Box Plot
   
-  if (isTRUE(data)){ p <- NULL # 4 scenarios: no dates, start, end & both dates
-  
+  if (data){ p <- NULL # 4 scenarios: no dates, start, end & both dates
+    
     src <- "yahoo"
     
     getData <- function(A, s, e) {
@@ -12,22 +12,35 @@ box.plt <- function(x, s = NULL, e = NULL, data = T, lg = F){ # Box Plot
       if (is.null(s)) return(getSymbols(A, to = e, src=src, auto.assign=F)) 
       return(getSymbols(A, from = s, to = e, src=src, auto.assign=F)) 
     }
-    for (A in x){ p <- cbind(p, getData(A, s, e)[,4]) } # Join data
-    
-    p <- p[apply(p, 1, function(x) all(!is.na(x))),] # Get rid of NA
-    
-    colnames(p) <- x # Put the tickers in column names
-    
-    x <- p } # Give column names 
+    for (A in x){ p <- cbind(p, getData(A, s, e)[,4]) # Join data
+      
+      message(
+        sprintf(
+          "%s is downloaded (%s / %s)", 
+          A, which(x == A), length(x)
+        )
+      )
+      }
+      p <- p[apply(p, 1, function(x) all(!is.na(x))),] # Get rid of NA
+      
+      colnames(p) <- x # Put the tickers in column names
+      
+      x <- p } # Give column names 
+      
+  if (lg | data) x <- diff(log(as.timeSeries(x)))[-1,] 
   
-  if (isTRUE(lg) || isTRUE(data)) { x <- diff(log(as.timeSeries(x)))[-1,] }
-  
-  boxplot.matrix(x, main = "Fluctuations of Securities", title = F, las = 1,
-                 col = "steelblue", xlab = "Data Source: Yahoo Finance")
+  boxplot.matrix(
+    x, 
+    main = "Fluctuations of Securities", 
+    title = F, 
+    las = 1,
+    col = "steelblue", 
+    xlab = "Data Source: Yahoo Finance"
+    )
   
   axis(side = 4, las = 2)
   
-  par(mar = c(5, 4, 4, 4)) # Define borders of the plot to fit right y-axis
+  par(mar = c(5, rep(4, 3))) # Define borders of the plot to fit right y-axis
   
   grid(nx = NULL, ny = NULL, col = "grey", lty = "dotted", lwd = 1)
   
