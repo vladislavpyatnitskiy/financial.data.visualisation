029b5d8df958a87d7a3e332673f4452297970dce
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2026-01-29 17:21:49 +0500

    Update Own Hist Script.R

--- Histogram/Own Hist Script.R
+++ Histogram/Own Hist Script.R
@@ -4,8 +4,8 @@ hist.plt <- function(x, s = NULL, e = NULL, lg = T, data = F){ # Histogram Plot
   
   L <- NULL
   
-  if (isTRUE(data)){ p <- NULL # data off
-  
+  if (data){ p <- NULL # data off
+    
     src <- "yahoo"
     
     getData <- function(A, s, e) {
@@ -14,28 +14,45 @@ hist.plt <- function(x, s = NULL, e = NULL, lg = T, data = F){ # Histogram Plot
       if (is.null(s)) return(getSymbols(A, to = e, src=src, auto.assign=F)) 
       return(getSymbols(A, from = s, to = e, src=src, auto.assign=F)) 
     }
-    for (A in x){ p <- cbind(p, getData(A, s, e)[,4]) } # Join data
+    for (A in x){ p <- cbind(p, getData(A, s, e)[,4]) 
+      
+      message(
+        sprintf(
+          "%s is downloaded (%s / %s)", 
+          A, which(x == A), length(x)
+        )
+      )
+    } # Join data
     
     p <- p[apply(p, 1, function(x) all(!is.na(x))),] # Get rid of NA
     
     colnames(p) <- x # Assign tickers
     
     x <- p } 
-  
-  if (isTRUE(lg) || isTRUE(data)){ x <- diff(log(x))[-1,] }
+    
+  if (lg | data) x <- diff(log(x))[-1,]
   
   for (n in 1:ncol(x)){ s <- x[,n] # For each column
   
-  h <- hist(s, main=sprintf("%s Histogram & Normal Distribution",colnames(s)),
-            ylab = "Likelihood", xlab = "Returns", xlim = c(min(s), max(s)),
-            col = "darkgreen", border = "white", breaks = 100, las=1, freq=F)
-  
-  grid(nx = NULL, ny = NULL, col = "grey", lty = "dotted", lwd = 1)
-  abline(v = 0, col = "lightblue", lwd = 2) # Add vertical line at 0
-  abline(h = 0)
-  
-  curve(dnorm(x, mean = mean(s), sd = sd(s)), col = "red", lwd = 3, add = T)
-  
-  box() } # Define borders
+    h <- hist(
+      s, 
+      main = sprintf("%s Histogram & Normal Distribution", colnames(s)),
+      ylab = "Likelihood", 
+      xlab = "Returns", 
+      xlim = c(min(s), max(s)),
+      col = "darkgreen", 
+      border = "white", 
+      breaks = 100, 
+      las = 1, 
+      freq = F
+      )
+    
+    grid(nx = NULL, ny = NULL, col = "grey", lty = "dotted", lwd = 1)
+    abline(v = 0, col = "lightblue", lwd = 2) # Add vertical line at 0
+    abline(h = 0)
+    
+    curve(dnorm(x, mean = mean(s), sd = sd(s)), col = "red", lwd = 3, add = T)
+    
+    box() } # Define borders
 }
 hist.plt(c("OMF", "HIG", "AAPL", "XOM"), s = "2020-01-01", data = T) # Test
