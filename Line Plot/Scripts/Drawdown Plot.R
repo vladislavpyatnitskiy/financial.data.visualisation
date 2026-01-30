eb7043b822bbf0485228d9a9af2d7dc159bf2a70
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2026-01-30 12:37:01 +0500

    Improve drawdown plot function formatting
    Refactor drawdown plotting function for clarity and formatting.

--- Line Plot/Scripts/Drawdown Plot.R
+++ Line Plot/Scripts/Drawdown Plot.R
@@ -1,8 +1,8 @@
-drawdown.plt <- function(x, SD = T){ # Function to plot asset drawdown
+drawdown.plt <- function(x, SD = T){ # Function to plot asset draw down
   
   x <- diff(log(x))[-1,] # Log returns
   
-  if (isFALSE(SD)){ r <- rownames(x) # Save dates for Cumulative Returns
+  if (!SD){ r <- rownames(x) # Save dates for Cumulative Returns
   
     x <- apply(x, 2, function(col) exp(cumsum(col)) - 1) # Cumulative Returns
     
@@ -14,15 +14,23 @@ drawdown.plt <- function(x, SD = T){ # Function to plot asset drawdown
   
   for (n in 1:ncol(x)){ s <- x[,n] # Plot each column in data frame
   
-    plot(s, type = "l", las = 2, ylim = c(min(s), 0), lwd = 1, col = "red",
-         xlab = "Trading Days", ylab = "Negative Returns (%)",
-         main = sprintf("%s Drawdown", colnames(s)))
+    plot(
+      s, 
+      type = "l", 
+      las = 2, 
+      ylim = c(min(s), 0), 
+      lwd = 1, 
+      col = "red",
+      xlab = "Trading Days", 
+      ylab = "Negative Returns (%)",
+      main = sprintf("%s Drawdown", colnames(s))
+      )
     
     grid(nx = 1, ny = NULL, col = "grey", lty = "dotted", lwd = 1)
     
     abline(h = 0)
     
-    par(mar = c(5, 4, 4, 4)) # Define borders of the plot to fit right y-axis
+    par(mar = c(5, rep(4, 3))) # Define borders of the plot to fit right y-axis
     
     axis(side = 4, las = 2) } # Right y-axis}
 }
