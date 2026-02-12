80573507e8cf9a69c19e53d8e11e649a6e6133f4
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2026-02-12 12:49:59 +0500

    Update Plot with MA.R
    • Rewrote script:
    
    • Added download message
    • Improved readability

--- Line Plot/Scripts/Plot with MA.R
+++ Line Plot/Scripts/Plot with MA.R
@@ -2,8 +2,8 @@ lapply(c("quantmod", "timeSeries"), require, character.only = T) # Library
 
 ma.plt <- function(x, s = NULL, e = NULL, data = T){ # Plot of Moving Averages
   
-  if (isTRUE(data)){ p <- NULL # Data download loop for 4 scenarios:
-  
+  if (data){ p <- NULL # Data download loop for 4 scenarios:
+    
     src <- "yahoo"
     
     getData <- function(A, s, e) {
@@ -12,20 +12,32 @@ ma.plt <- function(x, s = NULL, e = NULL, data = T){ # Plot of Moving Averages
       if (is.null(s)) return(getSymbols(A, to = e, src=src, auto.assign=F)) 
       return(getSymbols(A, from = s, to = e, src=src, auto.assign=F)) 
     }
-    for (A in x){ p <- cbind(p, getData(A, s, e)[,4]) } # Join data
+    for (A in x){ p <- cbind(p, getData(A, s, e)[,4]) 
+      
+      message(
+        sprintf(
+          "%s is downloaded (%s / %s)", A, which(x == A), length(x)
+        )
+      )
+    } # Join data
     
     p <- p[apply(p, 1, function(x) all(!is.na(x))),] # Remove NA
     
     colnames(p) <- x
     
-    x <- p } # Give tickers to data
-    
+    x <- p # Give tickers to data
+  }
+  
   r <- as.timeSeries(x) # Make data time series
   
   for (n in 1:ncol(r)){
     
-    chartSeries(round(r[,n], 2), theme = "white",
-                name = sprintf("%s Stock Performance", colnames(r[,n])),
-                TA = "addEMA(50, col='purple');addEMA(200, col='red')") }
+    chartSeries(
+      round(r[,n], 2), 
+      theme = "white",
+      name = sprintf("%s Stock Performance", colnames(r[,n])),
+      TA = "addEMA(50, col='purple');addEMA(200, col='red')"
+      ) 
+    }
 }
 ma.plt(x = "AIG", s = "2022-01-01", data = T) # Test
