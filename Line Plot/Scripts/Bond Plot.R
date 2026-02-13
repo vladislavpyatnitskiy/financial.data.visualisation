c4b8d5e48e756efc7480a7156acea427432fb904
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2026-02-13 14:01:22 +0500

    Modify bond plot for percentage display
    Updated plot parameters to include percentage in x-axis label and title.

--- Line Plot/Scripts/Bond Plot.R
+++ Line Plot/Scripts/Bond Plot.R
@@ -8,15 +8,19 @@ bond.y.plt <- function(P, C = 0, y, f = 1, l = 0, h = 100){
   
     p <- c(p, P * (C / .01 / n * (1 - d) + d)) }
   
-  plot(x = as.vector(seq(l, h) * .01),
-       y = p,
-       type = "l",
-       xlab = "Bond Yield",
-       ylab = "Bond Price",
-       main = "Bond Pricing Dependency On Yield",
-       col = "red",
-       lwd = 3,
-       las = 1) # Plot
+  par(mar = rep(5, 4)) # Margins
+  
+  plot(
+    x = as.vector(seq(l, h) * .01) * 100,
+    y = p,
+    type = "l",
+    xlab = "Bond Yield (%)",
+    ylab = "Bond Price",
+    main = "Bond Pricing Dependency On Yield (%)",
+    col = "red",
+    lwd = 3,
+    las = 1
+    ) # Plot
   
   axis(side = 4, las = 2) # Right y-axis
   
