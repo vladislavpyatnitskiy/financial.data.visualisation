6a26a91125a9a72e35d6fb935f44627c0d836cd7
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2026-03-17 11:30:58 +0500

    Update CVaR (Expected Shortfall) via VC Plot.R

--- Line Plot/Scripts/CVaR (Expected Shortfall) via VC Plot.R
+++ Line Plot/Scripts/CVaR (Expected Shortfall) via VC Plot.R
@@ -2,14 +2,16 @@ lapply(c("fGarch", "timeSeries", "quantmod"), require, character.only=T) # Libs
 
 CVaR.plt <- function(x, CVaR = c(95, 99, 99.9), lg = F){
   
-  if (isTRUE(lg)) { x = diff(log(x))[-1,] } # log returns 
+  if (lg) x = diff(log(x))[-1,] # log returns 
   
   for (n in 1:ncol(x)){ s <- x[,n] * 100 # For each column in data set
     
     t <- seq(nrow(s)) # Set index
     
     gm <- garchFit( ~ garch(1, 1), data = coredata(s), trace = F) # GARCH model
-    
+
+    par(mar = rep(5, 4))
+                       
     plot(t, s, type="l", xlab = "Trading Days", ylab = "Returns (%)", las = 1,
          col = "black", main = sprintf("%s VaR GARCH (1,1)", colnames(s)),
          sub = "Data Source: Yahoo! Finance") # Plot graph
@@ -19,8 +21,6 @@ CVaR.plt <- function(x, CVaR = c(95, 99, 99.9), lg = F){
     grid(nx = NULL, ny = NULL, col = "grey", lty = "dotted", lwd = 1)
     
     abline(h = 0) # Add horizontal lines
-    
-    par(mar = rep(5, 4))
     
     for (v in seq(CVaR)){ # Calculate values for Conditional Value-at-Risk
       
