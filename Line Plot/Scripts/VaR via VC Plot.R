852ada2a99a3454cbba3f34bdef4d2a1f8926fbc
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2026-03-17 11:29:59 +0500

    Refactor VaR plotting function for clarity

--- Line Plot/Scripts/VaR via VC Plot.R
+++ Line Plot/Scripts/VaR via VC Plot.R
@@ -2,14 +2,16 @@ lapply(c("fGarch", "timeSeries", "quantmod"), require, character.only=T) # Libs
 
 VaR.plt <- function(x, VaR = c(95, 99, 99.9), lg = F){ # Plot with VaR values
   
-  if (isTRUE(lg)) { x = diff(log(x))[-1,] } # log returns 
+  if (lg) x = diff(log(x))[-1,] # log returns 
   
   for (n in 1:ncol(x)){ s <- x[,n]  # For each column in data set
     
     t <- seq(nrow(s)) # Set index
     
     gm <- garchFit( ~ garch(1, 1), data = coredata(s), trace = F) # GARCH model
-    
+
+    par(mar = c(5, rep(4, 3))) } # Define borders of the plot to fit right y-axis
+  
     plot(t, s, type="l", xlab = "Trading Days", ylab = "Returns (%)", las = 1,
          col = "black", main = sprintf("%s VaR GARCH (1,1)", colnames(s)),
          sub = "Data Source: Yahoo! Finance") # Plot graph
@@ -22,7 +24,5 @@ VaR.plt <- function(x, VaR = c(95, 99, 99.9), lg = F){ # Plot with VaR values
     grid(nx = NULL, ny = NULL, col = "grey", lty = "dotted", lwd = 1)
     
     axis(side = 4, las = 2) # Right y-axis
-    
-    par(mar = c(5, 4, 4, 4)) } # Define borders of the plot to fit right y-axis
 }
 VaR.plt(stock_data, VaR = c(95, 97.5, 99), lg = T) # Test
